14a3f4104ef90f21d00ee048cb518915534abc6f
Marcell Kovács <marcell.kovacs123@gmail.com>
2024-04-07 16:41:24 +0200

    create.jsx_css

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -4,14 +4,14 @@ const Create = () => {
   return (
     <section id='create'>
         <div className='wrapper'>
-            <h2 id='input-head-title'>Add meg az URL-t!</h2>
+            <h1 id='input-head-title'>Az URL lerövidítése</h1>
             <br/>
             <div id='input-form-container'>
                 <form action="" id='long-url-input-form'>
                     
                     <input type="text" id='long-url-input-field'/>
                     
-                    <input type="submit" value='Create' id='submit-create'/>
+                    <input type="submit" value='Rövidít' id='submit-create'/>
                 </form>
             </div>
         </div>
