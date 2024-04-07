66b2cdf43921a942a55824e2751952f40b3d0236
Marcell Kovács <marcell.kovacs123@gmail.com>
2024-04-07 09:14:13 +0200

    0407_navbar.jsx elkészítve

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <nav>
+        <div className='wrapper'>
+            <span className='navlogo'>mytinyurl</span>
+            <ul>
+                <li><a href="#">Főoldal</a></li>
+                <li><a href="#">Rólunk</a></li>
+                <li><a href="#">Statisztikák</a></li>
+                <li><a href="#">Jogi nyilatkozat</a></li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default navbar
