d31b6b1f69e18627cdb7605c0a3b403a303cb6fc
Marcell Kovács <marcell.kovacs123@gmail.com>
2024-04-08 10:12:48 +0200

    circles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,24 +4,13 @@ const About = () => {
   return (
     <section id='about-section'>
         <div className='wrapper'>
-            <div id='about-text-contener'>
-                <h2>Rólunk</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Nisi laboriosam, sed, sint delectus dolore dolor quae enim, 
-                    labore fugiat obcaecati inventore earum voluptatem illo provident 
-                    asperiores mollitia tenetur dolorum adipisci. 
-                    Ex obcaecati voluptatibus suscipit rerum?
-                </p>
+            <div id='circle'>
+                Lorem ipsum dolor sit amet.
             </div>
-            <div id='bigest-circle'>
-                <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Nemo id veniam recusandae, inventore quis placeat!"</p>
+            <div className='container'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi dicta a beatae, alias libero cum, officiis ad incidunt animi excepturi voluptatem explicabo necessitatibus architecto autem! Natus minima vero quaerat odio maxime repellendus neque quo assumenda tempora et reiciendis magnam repellat debitis eum animi, blanditiis rem rerum, sit hic aliquid impedit a voluptatibus dolorem sequi? Laboriosam blanditiis ullam nobis, eligendi cumque nemo atque velit sunt, repellat dolorum dolores praesentium aliquid quidem sed a, accusamus debitis expedita. Nostrum aspernatur facilis, illo animi deserunt repellendus cum impedit quas, necessitatibus voluptatem facere in perspiciatis. Dignissimos atque repudiandae aut libero, tenetur nesciunt animi velit blanditiis, sint facilis eum saepe id officia ut? Voluptatum, delectus id est, quod explicabo sit aperiam laudantium earum vero magnam itaque sequi dolorem! Rem asperiores quasi dolorem maxime sapiente ad itaque, est adipisci. Beatae quos recusandae facilis id consectetur fugiat quasi architecto iste quisquam modi exercitationem aperiam, consequatur magni. Libero.</p>
             </div>
-            <div id='bigger-circle'>
-                <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Voluptatum, vitae?"</p>
-            </div>
-            <div id='small-circle'></div>
+            
             
         </div>
     </section>
