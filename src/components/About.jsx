830c627476d28fa1bb052ec33dbab459757a8d80
Marcell Kovács <marcell.kovacs123@gmail.com>
2024-04-08 12:38:02 +0200

    02

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,10 @@ const About = () => {
   return (
     <section id='about-section'>
         <div className='wrapper'>
-            <div id='circle'>
-                Lorem ipsum dolor sit amet.
+            <div className='circle'>
+                <blockquote id='white-circle-text'>
+
+                </blockquote>
             </div>
             <div className='container'>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi dicta a beatae, alias libero cum, officiis ad incidunt animi excepturi voluptatem explicabo necessitatibus architecto autem! Natus minima vero quaerat odio maxime repellendus neque quo assumenda tempora et reiciendis magnam repellat debitis eum animi, blanditiis rem rerum, sit hic aliquid impedit a voluptatibus dolorem sequi? Laboriosam blanditiis ullam nobis, eligendi cumque nemo atque velit sunt, repellat dolorum dolores praesentium aliquid quidem sed a, accusamus debitis expedita. Nostrum aspernatur facilis, illo animi deserunt repellendus cum impedit quas, necessitatibus voluptatem facere in perspiciatis. Dignissimos atque repudiandae aut libero, tenetur nesciunt animi velit blanditiis, sint facilis eum saepe id officia ut? Voluptatum, delectus id est, quod explicabo sit aperiam laudantium earum vero magnam itaque sequi dolorem! Rem asperiores quasi dolorem maxime sapiente ad itaque, est adipisci. Beatae quos recusandae facilis id consectetur fugiat quasi architecto iste quisquam modi exercitationem aperiam, consequatur magni. Libero.</p>
